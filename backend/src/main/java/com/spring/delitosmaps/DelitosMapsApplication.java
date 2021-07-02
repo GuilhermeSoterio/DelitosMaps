1faf7ebf02c558bf297226f2708b37293ca4f67c
Guilherme Sotério <guilhermehenrique.oliv3@gmail.com>
2021-07-02 07:52:48 -0300

    Project created

---
+++ backend/src/main/java/com/spring/delitosmaps/DelitosMapsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.delitosmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelitosMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelitosMapsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spring/delitosmaps/DelitosMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.delitosmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelitosMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
